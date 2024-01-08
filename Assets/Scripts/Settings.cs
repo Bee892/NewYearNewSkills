611f122679f5a3b8413a930c1bf752656c845847
Brandon Pero <toozer1@hotmail.com>
2024-01-08 12:36:48 -0500

    Created drag rotation and zooming in and out.

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Timeline.Actions;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Settings : ScriptableObject
+{
+    [Range(0.1f, 1)] public float RotationSpeed;
+    [Range(0.1f, 1)] public float ZoomSpeed;
+}
